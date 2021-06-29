1e0b399936b1a43802283d1c73147da082174c51
Mario <juniorm3@msn.com>
2021-06-29 11:31:05 -0300

    14.8. Cadastrando um contato

--- src/main/java/com/algaworks/ContatosControle.java
+++ src/main/java/com/algaworks/ContatosControle.java
@@ -1,9 +1,11 @@
 package com.algaworks;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -41,5 +43,16 @@ public class ContatosControle {
 		modelAndView.addObject("contato", new Contato());
 		
 		return modelAndView;
+	}
+	
+	@PostMapping("/contatos")
+	public String cadastrar(Contato contato) {
+		String id = UUID.randomUUID().toString();
+		
+		contato.setId(id);
+		
+		LISTA_CONTATOS.add(contato);
+		
+		return "redirect:/contatos";
 	}
 }
